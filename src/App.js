00a411c54b1397aabd07c7f664396d52569335c1
jaco.kotzee <jaco.kotzee@spandigital.coml>
2018-07-13 19:56:32 +0200

    added node-sass and sass-loader packages AND created first scss styles to test with app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./styles/app.scss";
 
 class App extends Component {
   render() {
     return (
-      <div>
-
+      <div className="app">
+        <p>Hi</p>
       </div>
     );
   }

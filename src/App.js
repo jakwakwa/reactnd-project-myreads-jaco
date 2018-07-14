db16228a5eed6d07e67139d9f8fa51dc9d152b42
jaco.kotzee <jaco.kotzee@spandigital.coml>
2018-07-14 13:12:05 +0200

    added CSSBaseline for base styles intogration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './styles/app.scss';
+import ReactDOM from 'react-dom';
+import CSSBaseline from '@material-ui/core/CssBaseline';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <p>Hi</p>
-      </div>
+      <React.Fragment>
+        <CSSBaseline />
+        {/* Insert rest of the application here */}
+      </React.Fragment>
     );
   }
 }

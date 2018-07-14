b98c7fb4d4acc0d3f1cdd72bef213078bcf331ab
jaco.kotzee <jaco.kotzee@spandigital.coml>
2018-07-14 13:18:48 +0200

    added fragment component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './styles/app.scss';
 import ReactDOM from 'react-dom';
 import CSSBaseline from '@material-ui/core/CssBaseline';
@@ -6,10 +6,10 @@ import CSSBaseline from '@material-ui/core/CssBaseline';
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <Fragment>
         <CSSBaseline />
         {/* Insert rest of the application here */}
-      </React.Fragment>
+      </Fragment>
     );
   }
 }

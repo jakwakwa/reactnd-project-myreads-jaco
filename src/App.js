6dc590103697faf38f4f6ab00daeeb4c018395e8
jaco.kotzee <jaco.kotzee@spandigital.coml>
2018-07-14 12:29:57 +0200

    added prettierrc configuration file and set configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import "./styles/app.scss";
+import React, { Component } from 'react';
+import './styles/app.scss';
 
 class App extends Component {
   render() {

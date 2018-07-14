0b0417791261e5acdfa8ae87035df20fa0f37c8f
jaco.kotzee <jaco.kotzee@spandigital.coml>
2018-07-14 19:18:21 +0200

    edit readme

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+import '../styles/app.scss';
+// import ReactDOM from 'react-dom';
+import Header from './Layouts/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Layouts/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../styles/Layouts/Header.scss';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="appBar">
+        <h1>MyReads</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;

0bf05866015fe029b2c21b03ec8b7e749edee8b1
jaco.kotzee <jaco.kotzee@spandigital.coml>
2018-07-16 08:07:38 +0200

    destructure Header component function arguments

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import '../../styles/Layouts/Header.scss';
 
-const Header = props => (
+const Header = ({ title }) => (
   <header className="appBar">
-    <h1>{props.title}</h1>
+    <h1>{title}</h1>
   </header>
 );
 

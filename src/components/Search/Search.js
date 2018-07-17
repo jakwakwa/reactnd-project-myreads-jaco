bd6609a524c7a18e6df7eba9a2be0aadd8802745
jaco.kotzee <jaco.kotzee@spandigital.coml>
2018-07-17 17:44:51 +0200

    bind Search component state property with input field value by creating an update method

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 import '../../styles/Search.scss';
 
 class Search extends Component {
+  state = {
+    query: ''
+  };
+  updateQuery = query => {
+    this.setState({ query: query.trim() });
+  };
   render() {
     return (
       <div className="search-books">
@@ -19,7 +25,13 @@ class Search extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={event => this.updateQuery(event.target.value)}
+            />
+            {JSON.stringify(this.state)}
           </div>
         </div>
         <div className="search-books-results">
